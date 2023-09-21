81dcf8c5f128b8a11e7163db035a64eb47caaa47
BT_1N3_18 <sample@bths.edu>
2023-09-21 08:44:19 -0400

    added tip percentage, total cost, and item name (untested)

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -3,6 +3,29 @@ import java.util.Scanner;
 public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Enter amountof people: ");
+        int numPeople = scan.nextInt();
+
+        System.out.print("Enter tip percentage: ");
+        double percentTip = scan.nextDouble();
+
+        double total = 0;
+        String items = "";
+        String itemName;
+        System.out.print("Enter price of item: ");
+        double price = scan.nextDouble();
+
+        while (price != -1) {
+
+            System.out.print("Enter item name: ");
+            itemName = scan.nextLine();
+            items += (itemName + "\n");
+            total += price;
+
+            System.out.print("Enter price of item: ");
+            price = scan.nextDouble();
+
+        }
 
 
     }
