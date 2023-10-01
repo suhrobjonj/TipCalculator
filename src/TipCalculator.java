77fa1815c201dd4b9c194a68ea895754f39c0942
jurassu <j.suxrrobjon55@gmail.com>
2023-09-30 22:43:52 -0400

    convenient variable names

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -14,12 +14,12 @@ public class TipCalculator {
         int percentTip = scan.nextInt();
 
         //initializing variables for the while loop
-        double total = 0;
-        String items = "\n";
+        double totalPriceBeforeTip = 0;
+        String itemList = "\n";
         String itemName;
         int itemCount;
         System.out.print("Enter price of item: ");
-        double price = scan.nextDouble();
+        double itemPrice = scan.nextDouble();
 
         /*
         while loop logic
@@ -27,37 +27,37 @@ public class TipCalculator {
         total price of item is added to total
         name and count of item is added to the items "list"
          */
-        while (price != -1) {
+        while (itemPrice != -1) {
             scan.nextLine();
             System.out.print("Enter item name: ");
             itemName = scan.nextLine();
-            System.out.print("Total number ordered: ");
+            System.out.print("Total number of " + itemName + " ordered: ");
             itemCount = scan.nextInt();
 
             // main calculations occur, itemCount is used in  both the total price and the item list
-            items += (itemName + " x " + itemCount + "\n");
-            total += price * itemCount;
+            itemList += (itemName + " x " + itemCount + "\n");
+            totalPriceBeforeTip += itemPrice * itemCount;
 
             System.out.print("Enter price of item (-1 to continue): ");
-            price = scan.nextDouble();
+            itemPrice = scan.nextDouble();
         }
 
         // calculating total from user input
 
         // printing lines along with performing certain operations
-        double totalTip = total * ((double) percentTip / 100);
+        double totalTip = totalPriceBeforeTip * ((double) percentTip / 100);
         System.out.println("-----------------------------------------------");
-        System.out.println("Total price: $" + String.format("%.2f",total));
+        System.out.println("Total price: $" + String.format("%.2f",totalPriceBeforeTip));
         System.out.println("Tip percentage: " + percentTip + "%");
         System.out.println("Total tip amount: $" + String.format("%.2f",totalTip));
-        System.out.println("Total bill after tip: $" + String.format("%.2f",(total + totalTip)));
-        System.out.println("Per person cost before tip: $" + String.format("%.2f",(total / numPeople)));
+        System.out.println("Total bill after tip: $" + String.format("%.2f",(totalPriceBeforeTip + totalTip)));
+        System.out.println("Per person cost before tip: $" + String.format("%.2f",(totalPriceBeforeTip / numPeople)));
         System.out.println("Per person tip cost: $" + String.format("%.2f",(totalTip / numPeople)));
-        System.out.println("Total cost per person: $" + String.format("%.2f",((total + totalTip) / numPeople)));
+        System.out.println("Total cost per person: $" + String.format("%.2f",((totalPriceBeforeTip + totalTip) / numPeople)));
         System.out.println("-----------------------------------------------");
 
         // printing item list
-        System.out.println("Items ordered: " + items);
+        System.out.println("Items ordered: " + itemList);
 
     }
 }
